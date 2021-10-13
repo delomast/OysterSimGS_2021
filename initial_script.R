b266f4470ac8f954b771421feb92d0c1bfa143bc
Tom Delomas <delomast@gmail.com>
2021-10-13 16:57:04 -0400

    rough prototype in progress - up to imputation

--- initial_script.R
+++ initial_script.R
@@ -2,4 +2,106 @@
 # the world is your oyster
 
 library(AlphaSimR)
+library(tidyverse)
+library(rrBLUP)
+
+source("utils.R")
+
+# parallel not used yet
+# suggested strategy is develop script, then run iterations of whole
+# script in parallel on an HPC
+# library(parallel)
+# library(parallelly)
+# numCores <- availableCores()
+# if(Sys.info()["sysname"] == "Windows") numCores <- 1
+
+# quick founder pop for prototyping
+founderPop <- runMacs2(
+		nInd = 200,
+		nChr = 10,
+		segSites = 5100,
+		Ne = 12000,
+		bp = 7e+07,
+		genLen = 1,
+		mutRate = 2.5e-08,
+		histNe = c(12000, 1e+05),
+		histGen = c(100, 1e+06),
+		inbred = FALSE,
+		split = NULL,
+		ploidy = 2,
+		returnCommand = FALSE,
+		nThreads = NULL
+)
+
+SP <- SimParam$new(founderPop)
+SP$addTraitA(nQtlPerChr=100) # lot's of QTL
+SP$setVarE(h2=0.3) # in the range of heritability for growth, meat yield, survival, etc
+SP$setSexes("yes_rand") # at the time of breeding, all individuals will only be one sex
+SP$addSnpChip(5000) # all non-QTL SNPs saved from simulation
+
+pop <- newPop(founderPop)
+snpGen <- pullSnpGeno(pop) # founder SNP genotypes
+
+# SNP panel "design"
+numLoci <- c(seq(10, 100, 10), seq(125, 500, 25), seq(750, 5000, 250), seq(6000, 50000, 2000)) # number of loci in each panel to compare
+
+numLoci <- numLoci[1:5] # for quick testing
+
+snpMap <- getSnpMap()
+
+# choose markers
+allPanels <- lapply(numLoci, quickChooseLoci, genos = snpGen, map = snpMap)
+
+# initial spawning
+offspring <- randCross(pop, nCrosses = 100, nProgeny = 60, balance = TRUE)
+
+# phenotype training pop (sibs)
+trainPhenos <- sibTestEqual(fam = offspring, propTest = 0.5)
+
+# now calculate things with a bunch of different panels
+res <- data.frame()
+for(i in 1:length(allPanels)){
+	# genotypes
+	g <- pullSnpGeno(offspring)[,allPanels[[i]]$id]
+	
+	# calc GEBVs - no imputation
+	Amat <- A.mat(g - 1) # centering and calculating G with first method of VanRaden (2008), also Endelman and Jannik (2012)
+	p <- data.frame(id = rownames(Amat)) |> 
+		left_join(trainPhenos |> select(id, Trait_1) |> rename(pheno = Trait_1), by = "id") # hard coded for first trait
+	# predict genomic breeding values
+	gebv <- kin.blup(data = p, geno = "id", pheno = "pheno", K = Amat)
+	comp <- data.frame(id = offspring@id, gv = gv(offspring)) |> 
+		left_join(data.frame(id = names(gebv$g), gebv = gebv$g), by = "id") |>
+		left_join(p, by = "id")
+	
+	# quick inspection of one iteration
+	# cor(comp$gv[is.na(comp$pheno)], comp$gebv[is.na(comp$pheno)])
+	# comp %>% ggplot() + aes(x = gebv, y = gv, color = is.na(pheno)) + geom_point() + 
+	# 	geom_smooth(method = "lm")
+	
+	# snpBLUP <- mixed.solve(p$pheno, Z = g - 1) # implementation of SNP-BLUP (note it is much faster for panel size << number of individuals)
+	
+	# calc accuracy of prediction and save
+	res <- res |> rbind(data.frame(panelNum = i, 
+																 impute = FALSE, 
+																 numLoci = ncol(g), 
+																 acc = cor(comp$gv[is.na(comp$pheno)], comp$gebv[is.na(comp$pheno)])))
+	
+	# impute
+	
+	# calc imputation accuracy and save
+	
+	# calculate GEBVs
+	
+	# calc accuracy of prediction and save
+	
+}
+
+
+# now testing imputation with deeper pedigree
+# make a few generations with random crosses then run above
+
+# impute and 
+
+
 
---
+++ utils.R
@@ -0,0 +1,72 @@
+# utility functions used in other script(s)
+
+library(tibble)
+library(dplyr)
+
+#' function to apply full-sib family testing with an AlphaSimR object
+#' pulls ID's and phenotypes for specified proportion (rounded) of individuals from each
+#' full-sib family
+#' 
+#' @param fam an AlphaSimR "Pop-class" object containing full-sib families
+#' @param propTest the proportion of each family to phenotype. One value applied
+#'   to all families. The actual number of indiviudals to phenotype will be rounded.
+sibTestEqual <- function(fam, propTest){
+	fs <- tibble(mother = fam@mother, father = fam@father) |> 
+		count(mother, father) |> mutate(nPheno = round(propTest * n))
+	phenos <- data.frame()
+	for(i in 1:nrow(fs)){
+		# select id
+		toPheno <- sample(fam@id[fam@mother == fs$mother[i] & fam@father == fs$father[i]], size = fs$nPheno[i])
+		temp <- cbind(toPheno, fs$mother[i], fs$father[i], as.data.frame(fam@pheno[match(toPheno, fam@id),]))
+		colnames(temp) <- c("id", "mother", "father", paste0("Trait_", 1:fam@nTraits))
+		phenos <- phenos |> rbind(temp)
+	}
+	return(phenos)
+}
+
+#' simple heuristic to choose markers for a genetic panel
+#' this is mainly for prototyping, more sophisticated processes should probably be used
+#' 
+#' moves a window step wise along each chromosome and picks the highest variability marker in that window.
+#' window size is calculated as numLoci/genetic length.
+#' equal number of SNPs per chromosome.
+#' some rounding may affect final number of SNPs.
+#' 
+#' @param num number of loci desired in the panel
+#' @param genos genotypes (output of `AlphaSimR::pullSnpGeno`)
+#' @param map genetic map of loci (output of `AlphaSimR::getSnpMap`)
+quickChooseLoci <- function(num, genos, map){
+	He <- (colSums(genos) / (2 * nrow(genos))) |> (\(x) 2 * x * (1-x))() # H_exp with R v4 tricks
+	map <- map |> left_join(tibble(He = He, id = names(He)), by = "id")
+	# calculate window sizes
+	distr <- map |> group_by(chr) |> summarise(l = max(pos)) |> 
+		mutate(num = round(num / n_distinct(chr)), wSize = l / num)
+	panel <- tibble()
+	for(i in 1:nrow(distr)){ # for each chr
+		skipBool <- FALSE
+		cands <- map |> filter(chr == distr$chr[i])
+		for(j in 1:distr$num[i]){ # for each desired SNP
+			start <- (j-1) * distr$wSize[i]
+			end <- start + distr$wSize[i]
+			while(TRUE){
+				chosen <- cands |> filter(pos > start & pos <= end) |> slice(which.max(He))
+				if(nrow(chosen) < 1){
+					# expand window if no SNPs are available
+					start <- start - distr$wSize[i] / 10
+					end <- end + distr$wSize[i] / 10
+					if(start < 0 && end > distr$l[i]){
+						warning("ran out of SNPs in chromosome ", distr$chr[i])
+						skipBool <- TRUE
+						break
+					}
+				} else {
+					# add SNP to panel
+					panel <- panel |> bind_rows(chosen)
+					break
+				}
+			}
+			if(skipBool) break
+		}
+	}
+	return(panel)
+}
