a39a0c3482f147dea80f67151d41352495d4d015
Tom Delomas <delomast@gmail.com>
2022-01-10 17:30:58 -0500

    add random seed argument

--- sim_oyster_genos_scrm.sh
+++ sim_oyster_genos_scrm.sh
@@ -8,6 +8,7 @@
 # 3 mutation rate per base, 
 # 4 expected number of recombinations per chromosome per generation,
 # 5 output file prefix
+# 6 random seed
 
 # activate conda environment containing scrm
 module load miniconda
@@ -23,5 +24,5 @@ for len in "${chrom_lengths[@]}"; do
 	t=$(awk "BEGIN {print 4*$2*$3*$len}")
 	# calculate 4*Ne*r
 	R=$(awk "BEGIN {print 4*$2*$4}")
-	scrm $1 1 -t $t -r $R $len -l 200r > "$5"chr"$i".txt
+	scrm $1 1 -t $t -r $R $len -l 200r -seed $6 > "$5"chr"$i".txt
 done
