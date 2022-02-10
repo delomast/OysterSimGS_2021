8782b9bdcbc836d209ec5163b3ed5728c18b3ecb
Tom Delomas <delomast@gmail.com>
2022-02-10 15:07:35 -0500

    shell script to run onlyCurGen_scrm.R sims

---
+++ curGen_SNPsim_arrary.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# run simulations with scrm for
+# performance of different SNP panel sizes
+# written for execution on Ceres
+#SBATCH --cpus-per-task=1  # ask for 1 cpu
+#SBATCH --mem=45G # Maximum amount of memory this job will be given
+#SBATCH --time=47:00:00 # ask that the job be allowed to run for 
+#SBATCH --array=1-200%75 #specify how many jobs in the array and limit number running concurrently (e.g. 1-96%40)
+#SBATCH --output=arrayScrm_%a.out # tell it where to store the output console text
+
+echo "My SLURM_JOB_ID: " $SLURM_JOB_ID
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+
+module load r/4.1.2
+
+# check for random seeds
+if [ ! -f randSeeds.txt ]; then
+    echo "randSeeds.txt does not exist."
+	exit 1
+fi
+
+# get random seed
+x=$(cat randSeeds.txt | sed -n ${SLURM_ARRAY_TASK_ID}p)
+echo "My random seed is: " $x
+
+# make temp directory
+mkdir /90daydata/oyster_gs_sim/temp"$SLURM_ARRAY_TASK_ID"
+
+# run simulation
+date
+echo "begin scrm"
+# simulate genotypes
+# 1 number of haplotypes
+# 2 Ne, 
+# 3 mutation rate per base, 
+# 4 expected number of recombinations per chromosome per generation,
+# 5 output file prefix
+# 6 random seed (consecutive seeds starting at $6 + 1 are used)
+# These parameters yield about 1 SNP / 50 bp, which is in line with estimates for Pacific oyster (Sauvage et a. 2007)
+# may be a little under for easter oysters, but the easter oyster study was a much smaller dataset
+bash ./sim_oyster_genos_scrm.sh 400 20000 0.00000004 1 /90daydata/oyster_gs_sim/temp"$SLURM_ARRAY_TASK_ID"/ $x
+date
+echo "end scrm"
+
+# randomSeed iterationNumber TemporaryLocalStorageDirectory
+Rscript onlyCurGen_scrm.R $x $SLURM_ARRAY_TASK_ID /90daydata/oyster_gs_sim/
+
+# remove temp directory
+rm -r /90daydata/oyster_gs_sim/temp"$SLURM_ARRAY_TASK_ID"
+
+echo "Done with simulation"
